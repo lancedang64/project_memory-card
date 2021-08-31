5372b148342a456d65fe82e1646546277da45a9b
Lance Dang <lancedang64@gmail.com>
2021-08-31 22:07:34 +1000

    Removed manual error throwing from functions

--- src/lib/functions/array/index.tsx
+++ src/lib/functions/array/index.tsx
@@ -2,7 +2,6 @@ import { Card } from 'src/lib/types';
 
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 export function getShuffledArr(arr: Card[]): Card[] {
-  if (!Array.isArray(arr)) throw Error('array parameter is invalid');
   const newArr = [...arr];
   let currentIndex = newArr.length,
     randomIndex;
@@ -21,13 +20,9 @@ export function getShuffledArr(arr: Card[]): Card[] {
 }
 
 export function addCard(card: Card, arr: Card[]): Card[] {
-  if (!arr) throw Error('array parameter is invalid!');
-  if (!card) throw Error('card parameter is invalid!');
   return [...arr, card];
 }
 
 export function removeCard(card: Card, arr: Card[]): Card[] {
-  if (!arr) throw Error('array parameter is invalid!');
-  if (!card || !card.name) throw Error('card parameter is invalid!');
   return arr.filter((item) => item.name !== card.name);
 }
