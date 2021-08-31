b755d7e863ba975edee755d1ffbfbc1bf00b6f05
Lance Dang <lancedang64@gmail.com>
2021-08-31 21:19:25 +1000

    Linted some files

--- src/lib/functions/array/index.js
+++ src/lib/functions/array/index.js
@@ -1,34 +1,31 @@
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 export function getShuffledArr(arr) {
-	if (!Array.isArray(arr)) throw Error('array parameter is invalid');
-	const newArr = [...arr];
-	let currentIndex = newArr.length,
-		randomIndex;
+  if (!Array.isArray(arr)) throw Error('array parameter is invalid');
+  const newArr = [...arr];
+  let currentIndex = newArr.length,
+    randomIndex;
 
-	// While there remain elements to shuffle...
-	while (currentIndex !== 0) {
-		// Pick a remaining element...
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex--;
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-		// And swap it with the current element.
-		[newArr[currentIndex], newArr[randomIndex]] = [
-			newArr[randomIndex],
-			newArr[currentIndex],
-		];
-	}
+    // And swap it with the current element.
+    [newArr[currentIndex], newArr[randomIndex]] = [newArr[randomIndex], newArr[currentIndex]];
+  }
 
-	return newArr;
+  return newArr;
 }
 
 export function addCard(card, arr) {
-	if (!arr) throw Error('array parameter is invalid!');
-	if (!card) throw Error('card parameter is invalid!');
-	return [...arr, card];
+  if (!arr) throw Error('array parameter is invalid!');
+  if (!card) throw Error('card parameter is invalid!');
+  return [...arr, card];
 }
 
 export function removeCard(card, arr) {
-	if (!arr) throw Error('array parameter is invalid!');
-	if (!card || !card.name) throw Error('card parameter is invalid!');
-	return arr.filter(item => item.name !== card.name);
+  if (!arr) throw Error('array parameter is invalid!');
+  if (!card || !card.name) throw Error('card parameter is invalid!');
+  return arr.filter((item) => item.name !== card.name);
 }
