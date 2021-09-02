0d2ae9b967d41d4e18d4d87d644c48a69e335906
Lance Dang <lancedang64@gmail.com>
2021-09-02 13:43:30 +1000

    Styled GameOver modal

--- src/components/Body/components/GameOverModal/index.tsx
+++ src/components/Body/components/GameOverModal/index.tsx
@@ -2,12 +2,19 @@ import React, { ReactElement } from 'react';
 import { ReactNode } from 'react';
 import styled from 'styled-components';
 
+interface MyProps {
+  flexDirection?: string;
+}
+
 const Container = styled.div`
   z-index: 3;
   position: absolute;
   top: 40%;
   background: blanchedalmond;
   padding: 50px 100px;
+  display: flex;
+  flex-direction: ${(props: MyProps): string => props.flexDirection || 'column'};
+  align-items: center;
 `;
 
 const BlackOverlay = styled.div`
@@ -19,32 +26,58 @@ const BlackOverlay = styled.div`
   opacity: 50%;
 `;
 
+const ButtonStyled = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+`;
+
+const AllMight = styled.img`
+  height: 200px;
+`;
+
+const AllMight2 = styled.img`
+  -webkit-transform: scaleX(-1);
+  transform: scaleX(-1);
+  height: 200px;
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 interface TypeProps {
   correct: number;
   leftOver: number;
   isMaxScore: boolean;
-  children?: ReactNode;
+  handleNewGame: () => void;
 }
 
-function GameOverModal({ correct, leftOver, isMaxScore, children }: TypeProps): ReactElement {
+function GameOverModal({ correct, leftOver, isMaxScore, handleNewGame }: TypeProps): ReactElement {
   return (
     <>
       <BlackOverlay></BlackOverlay>
-      <Container>
+      <Container flexDirection={isMaxScore ? 'row' : 'column'}>
         {isMaxScore ? (
           <>
-            <h2>Game Over!</h2>
-            <h2>Plus Ultraaaa!!!!</h2>
-            <p>Congratulations!!! You got everything right and achieved the maximum score!</p>
+            <AllMight src="../images/All_Might_Approve.png" />
+            <StyledDiv>
+              <h2>Game Over!</h2>
+              <h2>Plus Ultraaaa!!!! All Mights Approved!!</h2>
+              <p>Congratulations!!! You got everything right and achieved the maximum score!</p>
+              <ButtonStyled onClick={handleNewGame}>New Game!</ButtonStyled>
+            </StyledDiv>
+            <AllMight2 src="../images/All_Might_Approve.png" />
           </>
         ) : (
           <>
             <h2>Game Over!</h2>
             <p>Good job! You made {correct} correct choices! </p>
             <p>Only {leftOver} unique cards left over!</p>
+            <ButtonStyled onClick={handleNewGame}>New Game!</ButtonStyled>
           </>
         )}
-        {children}
       </Container>
     </>
   );
